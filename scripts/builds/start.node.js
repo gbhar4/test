c595bb17af20d54c4f0e2f8b6c0cdbcded1c8147
Gagan Bhardwaj <gbhardwaj4@sapient.com>
2018-04-12 14:34:57 -0400

    Node added

--- scripts/builds/start.node.js
+++ scripts/builds/start.node.js
@@ -12,21 +12,21 @@ var serverBuild = child.fork('scripts/builds/build.server.js');
 var desktopBuild = child.fork('scripts/builds/build.desktop.js');
 var mobileBuild = child.fork('scripts/builds/build.mobile.js');
 
-serverBuild.on('message', (m) => {
-  var nodemon = require('nodemon')({
-    script: 'dist/server/server.js'
-  });
+// serverBuild.on('message', (m) => {
+//   var nodemon = require('nodemon')({
+//     script: 'dist/server/server.js'
+//   });
 
-  nodemon.on('start', function () {
-    console.log(`Node Monitoring has started`);
-  }).on('quit', function () {
-    cssBuild.kill('SIGINT');
-    serverBuild.kill('SIGINT');
-    desktopBuild.kill('SIGINT');
-    mobileBuild.kill('SIGINT');
-    console.log('Node Monitoring has stopped');
-    process.exit();
-  }).on('restart', function (files) {
-    console.log('App restarted due to: ', files);
-  });
-});
+//   nodemon.on('start', function () {
+//     console.log(`Node Monitoring has started`);
+//   }).on('quit', function () {
+//     cssBuild.kill('SIGINT');
+//     serverBuild.kill('SIGINT');
+//     desktopBuild.kill('SIGINT');
+//     mobileBuild.kill('SIGINT');
+//     console.log('Node Monitoring has stopped');
+//     process.exit();
+//   }).on('restart', function (files) {
+//     console.log('App restarted due to: ', files);
+//   });
+// });
